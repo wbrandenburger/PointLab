6171f193b29ff0fccbfb6b287981aa033fa54fc3
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2017-07-20 12:53:23 +0200

    Working heap

--- tools/tools/utils/heap.h
+++ tools/tools/utils/heap.h
@@ -651,8 +651,7 @@ namespace utils
 			assert(heapvector[index_].heap_node != nullptr);
 
 			heapvector[index_].heap_node->value = value_;
-			
-			std::cout << heapvector[index_].index << std::endl;
+
 			if (!pushup(heapvector[index_].index))
 			{
 				pulldown(heapvector[index_].index);
