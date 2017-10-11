03d8db7fe295168a89171b486fa610d7713bc480
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2017-10-11 17:35:06 +0200

    Plotting functions

---
+++ tools/tools/utils/color.h
@@ -0,0 +1,89 @@
+/***********************************************************************
+* Software License Agreement (BSD License)
+*
+* Copyright 2017  Wolfgang Brandenburger. All rights reserved.
+*
+* THE BSD LICENSE
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+* OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+* NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+* THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*************************************************************************/
+
+
+#ifndef UTILS_COLOR_H_
+#define UTILS_COLOR_H_
+
+namespace utils
+{
+	/**
+		Computes a color scheme within the range of red and blue
+
+		@param[in,out] r_ Red
+		@param[in,out] g_ Green
+		@param[in,out] b_ Blue
+		@param[in,out] index_ Index
+		@param[in,out] number_of_colors_ Number of Colors 
+	*/
+	void colorSchemeRGB(int& r_, int& g_, int& b_, size_t index_, size_t number_of_colors_) {
+		
+		float index = (float)number_of_colors_ / 2;
+		float increment = index_ > index ? 1 / std::ceil(index) :  1 / std::floor(index);
+
+		if (index_ < index) {
+			r_ = (int) 255 - 255 * increment * (float) index_;
+			g_ = (int) 255 * increment * (float) index_;
+			b_ = 0;
+		}
+		else {
+			r_ = 0;
+			g_ = (int) 255 - 255 * increment * (float) index_;
+			b_ = (int) 255 * increment * (float) index_;
+		}
+	}
+
+	/**
+		Computes a color scheme within the range of red and blue
+
+		@param[in,out] r_ Red
+		@param[in,out] g_ Green
+		@param[in,out] b_ Blue
+		@param[in,out] index_ Index
+		@param[in,out] number_of_colors_ Number of Colors
+	*/
+	void colorSchemeRGB(float& r_, float& g_, float& b_, size_t index_, size_t number_of_colors_) {
+
+		float index = (float)number_of_colors_ / 2;
+		float increment = index_ > index ? 1 / std::ceil(index) : 1 / std::floor(index);
+
+		if (index_ < index) {
+			r_ = 1.0 - increment * (float) index_;
+			g_ = increment * (float) index_;
+			b_ = 0.0f;
+		}
+		else {
+			r_ = 0.0f;
+			g_ = 1.0 - increment * (float)index_;
+			b_ = increment * (float) index_;
+		}
+	}
+
+}
+#endif /* UTILS_COLOR_H_ */
